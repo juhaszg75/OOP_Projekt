5672b91cb192f70f95be4e18c7c5d3e3b070a83b
juhaszg75 <juhaszg75@gmail.com>
2025-05-06 07:34:13 +0200

    Autokolcsonzo Rendszer

--- Berles.py
+++ Berles.py
@@ -11,9 +11,7 @@ class Berles:  # class kulcsszóval deklarálok egy Berles nevű osztályt
     def set_felhasznalo_nev(self, new_felhasznalo_nev):  # definiálok egy setter metódust
         self._felhasznalo_nev = new_felhasznalo_nev  # a self._felhasznalo_nev osztály attribútumot beállítom a new_felhasznalo_new értékre
 
-    felhasznalo_nev = property(get_felhasznalo_nev,
-                               set_felhasznalo_nev)  # létrehozok egy felhasznalo_nev attribútumot és a property függvénnyel megadom, hogy a get_felhasznalo_nev és set_felhasznalo_nev tartozik hozzá
+    felhasznalo_nev = property(get_felhasznalo_nev, set_felhasznalo_nev)  # létrehozok egy felhasznalo_nev attribútumot és a property függvénnyel megadom, hogy a get_felhasznalo_nev és set_felhasznalo_nev tartozik hozzá
 
-    def __str__(
-            self):  # definiálok egy string metódust (dunder metódus), ami akkor fog meghívódni, amikor az osztálypéldányra ráhívok egy print-et
+    def __str__(self):  # definiálok egy string metódust (dunder metódus), ami akkor fog meghívódni, amikor az osztálypéldányra ráhívok egy print-et
         return f"{self.felhasznalo_nev} béreli: {self.auto.rendszam} ({self.auto.tipus}) - {self.datum} napon"  # megjeleníti a bérlés részleteit
